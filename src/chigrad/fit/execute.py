d3f8ab3c36eaedb7a985739f32af6e028be943d9
Christian <christian.kindler@physik.uni-regensburg.de>
2026-06-17 16:08:08 +0200

    Removed execute.py script, already implemented in minimise.py

--- src/chigrad/fit/execute.py
+++
@@ -1,8 +0,0 @@
-# File: chigrad/src/chigrad/execute.py
-import iminuit
-import numpy as np
-from typing import Callable, Literal, Optional
-
-from chigrad.log import message
-from chigrad.fit.minimise import ConvergenceError, minimise
-from chigrad.statistics.jackknife import jackknife_variance, jackknife_covariance 
